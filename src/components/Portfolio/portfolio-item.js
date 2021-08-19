4468d4a5139b72700fff7d81d365f08f86691ae9
Courtney Morris <shull.courtney@gmail.com>
2021-08-19 16:35:52 -0600

    Cleaned up portfolio items and brought in images

--- src/components/Portfolio/portfolio-item.js
+++ src/components/Portfolio/portfolio-item.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function(props) {
+        // Data that we'll need:
+        // -background image: thumb_image_url
+        // -logo
+        // -description
+        // -id: id
+
+        const { id, description, thumb_image_url, logo_url } = props.item;
     return (
         <div>
-        <h3>{props.title}</h3>
-        <h4>{props.url}</h4>
-
-        <Link to={`/portfolio/${props.slug}`}>Link</Link>
+            <img src={thumb_image_url} />
+            <img src={logo_url} />
+            <div>
+                {description}
+            </div>
+        <Link to={`/portfolio/${id}`}>Link</Link>
         </div>
     )
 }
