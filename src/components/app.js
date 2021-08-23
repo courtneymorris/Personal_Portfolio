7e7e5b33c7224f0af24bd1c5f6343f77b68bfdf7
Courtney Morris <shull.courtney@gmail.com>
2021-08-23 16:31:50 -0600

    Added login handlers

--- src/components/app.js
+++ src/components/app.js
@@ -15,29 +15,63 @@ import NoMatch from './pages/no-match';
 
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <Router>
-          <div>
-            <NavigationContainer />
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    }
 
-
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/auth" component={Auth} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
-              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </div>
-        </Router>
-
-
-      </div>
-    );
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this)
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this)
   }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+  
+    render() {
+      return (
+        <div className='container'>
+          <Router>
+            <div>
+              <NavigationContainer />
+
+
+
+              <Switch>
+                <Route exact path="/" component={Home} />
+
+                <Route
+                  path="/auth"
+                  render={props => (
+                    <Auth
+                      {...props}
+                      handleSuccessfulLogin={this.handleSuccessfulLogin}
+                      handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                    />
+                  )}
+                />
+                
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/blog" component={Blog} />
+                <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          </Router>
+
+
+        </div>
+      );
+    }
 }
