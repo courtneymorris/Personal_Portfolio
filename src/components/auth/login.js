e11a328be28cfcaf4d47db4106f80abb6cb08a39
Courtney Morris <shull.courtney@gmail.com>
2021-08-23 14:27:57 -0600

    Added Login component

---
+++ src/components/auth/login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+                <form>
+                    <input type="text"/>
+                    <input type="password"/>
+                </form>
+                
+                <div>Button</div>
+            </div>
+        )
+    }
+}
