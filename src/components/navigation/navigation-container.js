12f446fa3edd32cc569f7ab9aa9daee74df20e49
Courtney Morris <shull.courtney@gmail.com>
2021-08-24 14:36:42 -0600

    Finished logout functionality

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from 'axios';
+import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
 
 const NavigationComponent = (props) => {
@@ -9,9 +11,20 @@ const NavigationComponent = (props) => {
                     Blog
                 </NavLink>
             </div>
-        )
-    }
+        );
+    };
 
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", { withCredentials: true }).then(response => {
+            if (response.status === 200) {
+                props.history.push("/");
+                props.handleSuccessfulLogout();
+            }
+            return response.data;
+        }).catch(error => {
+            console.log("Error signing out", error);
+        })
+    };
 
     return (
         <div className="nav-wrapper">
@@ -34,14 +47,19 @@ const NavigationComponent = (props) => {
                     </NavLink>
                 </div>
 
-                {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+                {props.loggedInStatus === "LOGGED_IN" ? (
+                    dynamicLink("/blog", "Blog")
+                ) : null}
             </div>
 
             <div className="right-side">
-                Courtney Morris
+                COURTNEY MORRIS-SHULL
+                {props.loggedInStatus === 'LOGGED_IN' ? (
+                    <a onClick={handleSignOut}>Sign Out</a>
+                ) : null }
             </div>
         </div>
     );
 }
 
-export default NavigationComponent
+export default withRouter(NavigationComponent);
