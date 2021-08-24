fb6c653bc107eec0546ef7531c75dc7d380bf566
Courtney Morris <shull.courtney@gmail.com>
2021-08-24 16:29:03 -0600

    Built out portfolio sidebar list component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import PortfolioSidebarList from '../Portfolio/portfolio-sidebar-list';
 
 export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        axios.get("https://courtneymorris.devcamp.space/portfolio/portfolio_items", {
+            withCredentials: true
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            });
+        }).catch(error => {
+            console.log("Error in get portfolio items: ", error)
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
@@ -9,7 +36,7 @@ export default class PortfolioManager extends Component {
                 </div>
 
                 <div className="right-column">
-                    <h1>Portfolio sidebar...</h1>
+                    <PortfolioSidebarList data={this.state.portfolioItems}/>
                 </div>
             </div>
         )
