c14b0c0b2a0554d320c659f8ce521d66ad2d4e6d
Courtney Morris <shull.courtney@gmail.com>
2021-08-25 14:42:02 -0600

    fixed unique key prop warning

--- src/components/app.js
+++ src/components/app.js
@@ -79,7 +79,11 @@ export default class App extends Component {
 
   authorizedPages() {
     return [
-      <Route path="/portfolio-manager" component={PortfolioManager} />
+      <Route
+      key="portfolio-manager"
+      path="/portfolio-manager"
+      component={PortfolioManager}
+      />
     ]
   }
 
