d21b7ee276d5687082df5a8b81861b120db40d72
Courtney Morris <shull.courtney@gmail.com>
2021-08-27 09:06:41 -0600

    More work on dropzone

--- src/components/Portfolio/portfolio-form.js
+++ src/components/Portfolio/portfolio-form.js
@@ -24,6 +24,27 @@ export default class PortfolioForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.componentConfig = this.componentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+    this.handleBannerDrop = this.handleBannerDrop.bind(this);
+    this.handleLogoDrop = this.handleLogoDrop.bind(this);
+  }
+
+  handleThumbDrop() {
+    return {
+      addedfile: (file) => this.setState({ thumb_image: file }),
+    };
+  }
+
+  handleBannerDrop() {
+    return {
+      addedfile: (file) => this.setState({ banner_image: file }),
+    };
+  }
+
+  handleLogoDrop() {
+    return {
+      addedfile: (file) => this.setState({ logo: file }),
+    };
   }
 
   componentConfig() {
@@ -49,6 +70,10 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[url]", this.state.url);
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
+
+    if (this.state.thumb_image) {
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
 
     return formData;
   }
@@ -136,7 +161,18 @@ export default class PortfolioForm extends Component {
             <DropzoneComponent
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
-            ></DropzoneComponent>
+              eventHandlers={this.handleThumbDrop()}
+            />
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleBannerDrop()}
+            />
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleLogoDrop()}
+            />
           </div>
 
           <div>
--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,63 +1,68 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioSidebarList from '../Portfolio/portfolio-sidebar-list';
-import PortfolioForm from '../Portfolio/portfolio-form';
+import PortfolioSidebarList from "../Portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../Portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            portfolioItems: []
+    this.state = {
+      portfolioItems: [],
+    };
+
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    this.handleSuccessfulFormSubmission =
+      this.handleSuccessfulFormSubmission.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems),
+    });
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get(
+        "https://courtneymorris.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true,
         }
-
-        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
-        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
-    }
-
-    handleSuccessfulFormSubmission(portfolioItem) {
+      )
+      .then((response) => {
         this.setState({
-          portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+          portfolioItems: [...response.data.portfolio_items],
         });
-    }
+      })
+      .catch((error) => {
+        console.log("Error in getPortfolioItems: ", error);
+      });
+  }
 
-    handleFormSubmissionError(error) {
-        console.log("handleFormSubmissionError error", error)
-    }
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
-    getPortfolioItems() {
-        axios.get("https://courtneymorris.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
-            withCredentials: true
-        }).then(response => {
-            this.setState({
-                portfolioItems: [...response.data.portfolio_items]
-            });
-        }).catch(error => {
-            console.log("Error in getPortfolioItems: ", error)
-        });
-    }
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleFormSubmissionError}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
+        </div>
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
-
-    render() {
-        return (
-            <div className="portfolio-manager-wrapper">
-                <div className="left-column">
-                    <PortfolioForm
-                        handleSuccessfulFormSubmission={this.handleFormSubmissionError}
-                        handleFormSubmissionError={this.handleFormSubmissionError}
-                    />
-                </div>
-
-                <div className="right-column">
-                    <PortfolioSidebarList
-                    data={this.state.portfolioItems}
-                    />
-                </div>
-            </div>
-        )
-    }
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
 }
