5f839bfdce8de65a89d9b7af843effc51e57ef68
Courtney Morris <shull.courtney@gmail.com>
2021-08-30 13:59:51 -0600

    Started work on blog component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
+export default class Blog extends Component {
+  constructor() {
+    super();
 
-            <div>
-                <Link to="/about">Read more about me</Link>
-            </div>
-        </div>
-    )
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://courtneymorris.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map((blogItem) => {
+      return <h1>{blogItem.title}</h1>;
+    });
+    return <div>{blogRecords}</div>;
+  }
 }
