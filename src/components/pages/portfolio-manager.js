3c79bb302ff7223543ea12051df149975630bf8c
Courtney Morris <shull.courtney@gmail.com>
2021-08-29 18:34:44 -0600

    Portfolio Manager completed

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -13,11 +13,18 @@ export default class PortfolioManager extends Component {
       portfolioToEdit: {},
     };
 
-    this.handleSuccessfulFormSubmission =
-      this.handleSuccessfulFormSubmission.bind(this);
+    this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+    this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.clearPortfolioToEdit = this.clearPortfolioToEdit.bind(this);
+  }
+
+  clearPortfolioToEdit() {
+    this.setState({
+      portfolioToEdit: {},
+    });
   }
 
   handleEditClick(portfolioItem) {
@@ -46,7 +53,11 @@ export default class PortfolioManager extends Component {
       });
   }
 
-  handleSuccessfulFormSubmission(portfolioItem) {
+  handleEditFormSubmission() {
+    this.getPortfolioItems();
+  }
+
+  handleNewFormSubmission(portfolioItem) {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems),
     });
@@ -83,8 +94,11 @@ export default class PortfolioManager extends Component {
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
           <PortfolioForm
-            handleSuccessfulFormSubmission={this.handleFormSubmissionError}
+            handleNewFormSubmission={this.handleNewFormSubmission}
+            handleEditFormSubmission={this.handleEditFormSubmission}
             handleFormSubmissionError={this.handleFormSubmissionError}
+            clearPortfolioToEdit={this.clearPortfolioToEdit}
+            portfolioToEdit={this.state.portfolioToEdit}
           />
         </div>
 
