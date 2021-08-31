ae0f8cfc62b5de118129690396dfb811a86d1b44
Courtney Morris <shull.courtney@gmail.com>
2021-08-31 16:41:54 -0600

    Connected blog to API via modal

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,17 +1,68 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogForm extends Component {
   constructor(props) {
-    super();
+    super(props);
 
-    this.state = {};
+    this.state = {
+      title: "",
+      blog_status: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_blog[title]", this.state.title);
+    formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+
+    return formData;
+  }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://courtneymorris.devcamp.space/portfolio/portfolio_blogs",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.props.handleSuccessfulFormSubmission(response.data);
+      })
+      .catch((error) => {
+        console.log("handleSubmit for blog error", error);
+      });
+
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
     return (
-      <form>
-        <input type="text" />
-        <input type="text" />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="title"
+          placeholder="Blog Title"
+          value={this.state.title}
+        />
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="blog_status"
+          placeholder="Blog status"
+          value={this.state.blog_status}
+        />
 
         <button>Save</button>
       </form>
--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -22,6 +22,13 @@ export default class BlogModal extends Component {
         backgroundColor: "rgba(1, 1, 1, 0.75)",
       },
     };
+
+    this.handleSuccessfulFormSubmission =
+      this.handleSuccessfulFormSubmission.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(blog) {
+    console.log("blog from blog form", blog);
   }
 
   render() {
@@ -34,7 +41,9 @@ export default class BlogModal extends Component {
           }}
           isOpen={this.props.modalIsOpen}
         >
-          <BlogForm />
+          <BlogForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+          />
         </ReactModal>
       </div>
     );
