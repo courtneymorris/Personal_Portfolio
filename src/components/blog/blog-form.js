1cf358caa3ded725851fcd714e781b0a663d2a9c
Courtney Morris <shull.courtney@gmail.com>
2021-08-31 21:47:04 -0600

    More modal stuff

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -31,7 +31,12 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfulFormSubmission(response.data);
+        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+
+        this.setState({
+          title: "",
+          blog_status: "",
+        });
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -23,6 +23,15 @@ export default class Blog extends Component {
     window.addEventListener("scroll", this.onScroll, false);
     this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
     this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleSuccessfulNewBlogSubmission =
+      this.handleSuccessfulNewBlogSubmission.bind(this);
+  }
+
+  handleSuccessfulNewBlogSubmission(blog) {
+    this.setState({
+      blogModalIsOpen: false,
+      blogItems: [blog].concat(this.state.blogItems),
+    });
   }
 
   handleModalClose() {
@@ -93,6 +102,9 @@ export default class Blog extends Component {
     return (
       <div className="blog-container">
         <BlogModal
+          handleSuccessfulNewBlogSubmission={
+            this.handleSuccessfulNewBlogSubmission
+          }
           handleModalClose={this.handleModalClose}
           modalIsOpen={this.state.blogModalIsOpen}
         />
